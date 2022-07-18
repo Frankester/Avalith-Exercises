2549a98bf18143ea670a158e5aeba1a4a2169f74
Frankester <francotomascallero@gmail.com>
2022-07-18 12:57:19 -0300

    :bug: Fix: adding index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+const myName = 'Franco Callero'
+const myAge = 21
+const isInCouple = false
+const numbersArray = [1,2,3,4,5,6,7,8,9,10]
+const namesArray = ['Franco Y', 'Débora Alvarado', 'Williams Hernández', 'Matias Leal', 'Guillermo Scharf']
+
+const data = {
+    name: 'Franco Callero',
+    age: 21,
+    inCouple: false,
+    numbers: [1,2,3,4,5,6,7,8,9,10],
+    skillersNames: ['Franco Y', 'Débora Alvarado', 'Williams Hernández', 'Matias Leal', 'Guillermo Scharf'],
+}
+
+console.log(data)
